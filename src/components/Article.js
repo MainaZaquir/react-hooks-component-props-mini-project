954d56d119bfbb74a2455c2975aa5716ca029371
Zaquir <108782708+MainaZaquir@users.noreply.github.com>
2023-11-15 12:07:07 +0300

    Updated Article.js

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Article=({title,date="January 1, 1970", preview,minutes})=>{
+    let fives =  (Math.ceil(minutes/5)*5)/5;
+    let tens =  (Math.ceil(minutes/10)*10)/10;
+    
+    return(
+        <article>
+            <h3>{title}</h3>
+            <small>{date}. {minutes <30 ? ("☕️".repeat(fives)):("🍱".repeat(tens))}{minutes} min read</small>
+            <p>{preview}</p>
+
+        </article>
+    )
+}
+export default Article;
